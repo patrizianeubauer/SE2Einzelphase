62acfa612092c9bdc69a7a2cae69dda641c7e76d
patzineubi <105806213+patzineubi@users.noreply.github.com>
2023-03-05 23:06:51 +0100

    GUI implementation

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAbschicken;
+    private TextView tvResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAbschicken = findViewById(R.id.btnAbschicken);
+        tvResponse = findViewById(R.id.tvServerResponse);
+
+        btnAbschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
